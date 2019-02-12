0137fe23628f9879b8b0ce89d496fbfbbe8da0a6
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-02-12 10:18:08 +0100

    WIP: tag rank pass

--- libs/ranktagger/tagrank_pass.cpp
+++ libs/ranktagger/tagrank_pass.cpp
@@ -1,4 +1,6 @@
 
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -8,10 +10,62 @@ using namespace llvm;
 
 namespace {
   struct TagRankPass : public PassInfoMixin<TagRankPass> {
-    PreservedAnalyses run (Function &F, FunctionAnalysisManager &FAM) {
-      if (F.hasName()) {
-        errs() << "Tag Rank" << F.getName() << "\n";
+    PreservedAnalyses run (Module &M, ModuleAnalysisManager &MAM) {
+      // errs() << "Tag Rank Pass runs on module.\n";
+      // errs() << "\t Module name: " << M.getName() << "\n";
+
+      // errs() << "\t Instrs count: " << M.getInstructionCount() << "\n";
+
+      for (auto& f : M.functions()) {
+        if (f.hasName()) {
+          // errs() << "Function: " << f.getName() << "\n";
+          if (f.getName() == "main") {
+            for (auto &bb : f.getBasicBlockList()) {
+              // errs() << " BB: " << bb.getName() << "\n";
+              for (Instruction &inst : bb.getInstList()) {
+                if (inst.getOpcode() == Instruction::Call) {
+                  auto &ci = cast<CallInst>(inst);
+                  const Function *called_fn = ci.getCalledFunction();
+                  if (called_fn->getName() == "MPI_Comm_rank") {
+                    errs() << "Called FUNCTION: " << *called_fn << "\n";
+                    errs() << "I'm calling Comm rank\n";
+                    errs() << "instr: " << ci << "\n";
+                    errs() << "#arg operands: " << ci.getNumArgOperands() << "\n";
+                    auto *v = ci.getArgOperand(1); // get rank??
+                    errs() << "V: " << *v << "\n";
+
+                    for (auto *u : v->users()) {
+                      errs() << "user: " << *u << "\n";
+                    }
+                    // errs() << "V: " << *v << "\n";
+                    // for (auto *u : v->users()) {
+                    //   errs() << "u: " << *u << "\n";
+                    //   for (auto &op : u->operands()) {
+                    //     errs() << "u-Op: " << *op << "\n";
+                    //   }
+                    //   errs() << "\n";
+                    //   // if (!inst.isIdenticalTo(u->get())) {
+                    //   //   errs() << "user of V: " << *u << "\n";
+                    //   // }
+                    // }
+                    // for (auto *u : v->users()) {
+                    //   for (auto &op : u->operands()) {
+                    //     errs() << "oop: " << op << "\n";
+                    //   }
+                    // }
+                    // errs() << "# uses: " << v->getNumUses() << "\n";
+                    // errs() << "# operands: " << v- << "\n";
+                    // for (auto &attr : ci.getAttributes()) {
+                    //   errs() << "Attr: " << attr.getAsString() << "\n";
+                    // }
+                  }
+                }
+              }
+            }
+          }
+        }
       }
+
       return PreservedAnalyses::all();
     }
   };
@@ -23,9 +77,9 @@ llvmGetPassPluginInfo() {
     LLVM_PLUGIN_API_VERSION, "TagRankPass", "v0.1", // TODO: expose version
       [](PassBuilder &PB) {
       PB.registerPipelineParsingCallback(
-        [](StringRef PassName, FunctionPassManager &FPM, ArrayRef<PassBuilder::PipelineElement>) {
+        [](StringRef PassName, ModulePassManager &MPM, ArrayRef<PassBuilder::PipelineElement>) {
           if (PassName == "tag-rank-pass") {
-            FPM.addPass(TagRankPass());
+            MPM.addPass(TagRankPass());
             return true;
           }
           return false;

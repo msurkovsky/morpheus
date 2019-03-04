9a80a72143a33337a751ca6838f5a03f2e5110d7
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-03-04 15:10:33 +0100

    WIP: making iterator over scope

--- include/morpheus/Analysis/MPIScopeAnalysis.hpp
+++ include/morpheus/Analysis/MPIScopeAnalysis.hpp
@@ -10,28 +10,45 @@
 
 #include <vector>
 #include <string>
+#include <iterator>
 
 namespace llvm {
 
   class MPIScopeAnalysis;
 
-  struct MPIScopeResult { // TODO: make the data private
-    Function *scope; // it has to be a function ("worst case" is main)
-    CallInst *start; // Q?: This is a pointer on call that may be either
-                     //     an MPI_Init call or another mediately calling MPI_Init.
-                     //     The Question is how to cope with it? ... Or what to expect
-    CallInst *end;
+  class MPIScopeResult { // TODO: make the data private
+    Function *scope; // NOTE: it has to be a function ("worst case" is main)
+    CallInst *start; // call that invokes MPI_Init within 'scope'
+    CallInst *end;   // call that invokes MPI_Finalize within 'scope'
 
-    CallInst *init;
-    CallInst *finalize;
+    CallInst *init_call;     // call of MPI_Init
+    CallInst *finalize_call; // call of MPI_Finalize
 
+  public:
+    MPIScopeResult() : scope(nullptr), start(nullptr), end(nullptr),
+                       init_call(nullptr), finalize_call(nullptr) { };
+
+    MPIScopeResult(Function *scope, CallInst *start, CallInst *end,
+                   CallInst* init_call, CallInst *finalize_call)
+      : scope(scope), start(start), end(end),
+        init_call(init_call), finalize_call(finalize_call) { }
+
+    bool isValid() {
+      // Instruction *test_i = init_call->next();
+      auto *bb = init_call->getParent();
+      auto it = bb->begin();
+      errs() << "Iterator: " << *it << "\n";
+      // auto &it = init_call->iterator;
+      return scope != nullptr;
+    }
     // NOTE:
     // Well, in the end I need a kind of navigation trough the scope. Therefore, a set of queries needs to be defined.
 
     // TODO: define it as an iterator over "unfolded" scope
   };
 
-  class MPIScopeAnalysis : public AnalysisInfoMixin<MPIScopeAnalysis> { // both -*-module-*- and function analysis
+
+  class MPIScopeAnalysis : public AnalysisInfoMixin<MPIScopeAnalysis> { // both -*-module-*- and function analysis (->4.3.19: I don't think so. *module* is enough)
     static AnalysisKey Key;
     friend AnalysisInfoMixin<MPIScopeAnalysis>;
 
--- libs/Analysis/tagrank_pass.cpp
+++ libs/Analysis/tagrank_pass.cpp
@@ -27,9 +27,10 @@ namespace {
       //   auto &res = fam.getResult<RankAnalysis>(f);
       // }
       auto &res = am.getResult<MPIScopeAnalysis>(m);
-      errs() << "Scope: " << res.scope->getName() << "\n";
-      errs() << "begin: " << res.start->getCalledFunction()->getName() << "\n";
-      errs() << "end: " << res.end->getCalledFunction()->getName() << "\n";
+      errs() << "Is scope valid: " << (res.isValid() ? "YES" : "NO") << "\n";
+      // errs() << "Scope: " << res.scope->getName() << "\n";
+      // errs() << "begin: " << res.start->getCalledFunction()->getName() << "\n";
+      // errs() << "end: " << res.end->getCalledFunction()->getName() << "\n";
 
 
       errs() << "TAG RANK: after\n";

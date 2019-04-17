8e5abfc0b310b5ca73954131cd3e71b9d5c08291
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-04-17 21:02:39 +0200

    WIP: scope analysis

--- include/morpheus/Analysis/MPIScopeAnalysis.hpp
+++ include/morpheus/Analysis/MPIScopeAnalysis.hpp
@@ -1,16 +1,19 @@
 
 //===----------------------------------------------------------------------===//
 //
+// MPIScopeAnalysis
+//
 // Description of the file ... basic idea to provide a scope of elements
 // that are involved in MPI either directly or indirectly
 //
 //===----------------------------------------------------------------------===//
 
-#include "llvm/IR/PassManager.h"
-#include "llvm/IR/SymbolTableListTraits.h"
-#include "llvm/IR/ValueMap.h"
+#ifndef MR_MPI_SCOPE_H
+#define MR_MPI_SCOPE_H
+
 #include "llvm/ADT/ilist_iterator.h"
 #include "llvm/ADT/simple_ilist.h"
+#include "llvm/IR/PassManager.h"
 
 #include <vector>
 #include <string>
@@ -118,34 +121,24 @@ namespace llvm {
     bool operator!=(const ScopeIterator &other) const {
       return !operator==(other);
     }
-  }; // end of ScopeIterator
+  }; // ScopeIterator
 
-
-  class MPIScopeAnalysis;
 
   // TODO: does it make sense to implement ilist_node_with_parent (as same as basic block?)
-  class MPIScopeResult { // TODO: make the data private
+  class MPIScope {
 
-    Function *scope; // NOTE: it has to be a function ("worst case" is main)
-    CallInst *start_f; // call that invokes MPI_Init within 'scope'
-    CallInst *end_f;   // call that invokes MPI_Finalize within 'scope'
-
-    CallInst *init_call;     // call of MPI_Init
-    CallInst *finalize_call; // call of MPI_Finalize
+    Module &analyzed_m;
+    std::unique_ptr<CallGraph> cg;
+    std::unique_ptr<MPILabeling> labelling;
 
   public:
     using iterator = ScopeIterator;
 
-    MPIScopeResult() : scope(nullptr), start_f(nullptr), end_f(nullptr),
-                       init_call(nullptr), finalize_call(nullptr) { };
+    explicit MPIScope(Module &m);
+    MPIScope(const MPIScope &scope);
+    MPIScope(MPIScope &&scope) = default;
 
-    MPIScopeResult(Function *scope, CallInst *start_f, CallInst *end_f,
-                   CallInst* init_call, CallInst *finalize_call)
-      : scope(scope), start_f(start_f), end_f(end_f),
-        init_call(init_call), finalize_call(finalize_call) { }
-
-    MPIScopeResult(const MPIScopeResult &sci) = default;
-
+    // TODO: review
     ScopeIterator begin() {
       return ScopeIterator(init_call, finalize_call).begin();
     }
@@ -157,54 +150,16 @@ namespace llvm {
     // ScopeIterator::const_iterator begin() const { iter.begin(); }
     // ScopeIterator::const_iterator end() const { iter.end(); }
 
-    bool isValid() {
-      return scope != nullptr;
-      // Instruction *test_i = init_call->next();
-      // auto node = init_call->getNextNode();
-      errs() << "\n\tInit call instr: " << *init_call << "\n";
+    bool isValid();
 
-      errs() << "\tisa<Inst>: " << isa<Instruction>(init_call) << "\n";
-      // ---------------->>>
-      auto next = init_call->getNextNode(); // TODO: It seems that getNextNode is the right instruction I'm looking for!!
-      errs() << "\tNext: " << *next << "\n";
-      // ----------------<<<
-
-      auto *bb = init_call->getParent();
-      // const auto &list = bb->*(BasicBlock::getSublistAccess((Instruction *) nullptr));
-      // const auto &list = bb->*(BasicBlock::getSublistAccess(init_call));
-
-
-      // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-      auto it2 =  init_call->getIterator(); // this is the solution I want to use!
-      // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-      // NOTE: the `end` is either nullptr or finalize_call
-
-
-      // auto next2 = list.getNextNode(init_call);
-      // errs() << "\tNxtC: " << *next2 << "\n";
-      auto it = bb->begin();
-      errs() << "\tIterator: " << *it << "\n";
-      errs() << "\tIt2:     " << *it2 << "\n";
-      auto next2 = std::next(it2);
-      errs() << "\tIt next2: " << *next2 << "\n";
-      // auto &it = init_call->iterator;
-      return scope != nullptr;
-    }
     // NOTE:
     // Well, in the end I need a kind of navigation trough the scope. Therefore, a set of queries needs to be defined.
 
     // TODO: define it as an iterator over "unfolded" scope
-  };
+  }; // MPIScope
 
-  enum struct ExplorationState {
-    PROCESSING = 0,
-    SEQUENTIAL,
-    MPI_CALL,
-    MPI_INVOLVED,
-    MPI_INVOLVED_MEDIATELY,
-  }; // TODO: place it on a better place
 
-  class MPIScopeAnalysis : public AnalysisInfoMixin<MPIScopeAnalysis> { // both -*-module-*- and function analysis (->4.3.19: I don't think so. *module* is enough)
+  class MPIScopeAnalysis : public AnalysisInfoMixin<MPIScopeAnalysis> {
     static AnalysisKey Key;
     friend AnalysisInfoMixin<MPIScopeAnalysis>;
 
@@ -213,5 +168,7 @@ namespace llvm {
     using Result = MPIScopeResult;
 
     MPIScopeResult run (Module &, ModuleAnalysisManager &);
-  };
-}
+  }; // MPIScopeAnalysis
+} // llvm
+
+#endif // MR_MPI_SCOPE_H

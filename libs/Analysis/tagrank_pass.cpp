69dd1f100cfd7f7331477a71a11e00d9e2325f65
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-05-08 17:41:53 +0200

    WIP: cl::opt

--- libs/Analysis/tagrank_pass.cpp
+++ libs/Analysis/tagrank_pass.cpp
@@ -1,27 +1,23 @@
 
+#include "llvm/Analysis/ModuleSummaryAnalysis.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
-#include "llvm/Analysis/ModuleSummaryAnalysis.h"
+
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include "morpheus/Analysis/MPILabellingAnalysis.hpp"
 #include "morpheus/Analysis/MPIScopeAnalysis.hpp"
 
 using namespace llvm;
 
-// static cl::opt<unsigned> UserBonusInstThreshold(
-//     "bonus-inst-threshold", cl::Hidden, cl::init(1),
-//     cl::desc("Control the number of bonus instructions (default = 1)"));
-
-// static unsigned my_var;
-static cl::opt<bool> test("abc", cl::init(false));
+static cl::opt<bool>
+TagRank("tagrank", cl::desc("Print pass management debugging information"));
 
 namespace {
 
   struct TagRankPass : public PassInfoMixin<TagRankPass> {
-
-    TagRankPass() : rank(test.getNumOccurrences() ? -1 : 1) { }
 
     PreservedAnalyses run (Module &m, ModuleAnalysisManager &am) {
 
@@ -40,31 +36,30 @@ namespace {
 
       return PreservedAnalyses::all();
     }
-
-  private:
-    int rank;
   };
 } // end of anonymous namespace
 
 
 // The possibility to call pass via opt
+
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 
 llvmGetPassPluginInfo() {
-  return {
+  return { // creates and return PassPluginInfo object
     LLVM_PLUGIN_API_VERSION, "TagRankPass", "v0.1", // TODO: expose version
       [](PassBuilder &PB) {
       PB.registerPipelineParsingCallback(
         [](StringRef PassName, ModulePassManager &MPM, ArrayRef<PassBuilder::PipelineElement>) {
-          if (PassName == "tag-rank-pass") {
+          errs() << "PassName: " << PassName << "\n";
+          if (PassName == "tagrank") {
             // NOTE: the standard passes are already registered
             //       so I just add them, if needed.
             MPM.addPass(RequireAnalysisPass<CallGraphAnalysis, Module>());
             MPM.addPass(RequireAnalysisPass<ModuleSummaryIndexAnalysis, Module>());
             MPM.addPass(TagRankPass());
-            return true;
           }
-          return false;
+          return true;
+          // return false; // TODO: that's make a mistake of disability to parse pass pipeline
         }
       );
     }

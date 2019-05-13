6cb0b77ab660a485c3c6cd893d004d20081a6e45
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-05-13 07:11:12 +0200

    WIP: replace rank with a constant

--- libs/Transforms/MPIPruneProcess.cpp
+++ libs/Transforms/MPIPruneProcess.cpp
@@ -10,25 +10,40 @@ using namespace llvm;
 // -------------------------------------------------------------------------- //
 // MPIPruneProcessPass
 
-// NOTE: no the problem remains the same, but why?
-static cl::opt<unsigned> targ("mujarg",
-                              cl::init(7),
-                              cl::desc("Some description of the testing argument"));
+static cl::opt<unsigned> rank_arg(
+    "rank", cl::Required, cl::Hidden,
+    cl::desc("An unsigned integer specifying rank of interest."));
 
 PreservedAnalyses MPIPruneProcessPass::run (Module &m, ModuleAnalysisManager &am) {
-  errs() << "TAG RANK: before\n";
 
-  // All of my passes needs to be registered before used.
   am.registerPass([] { return MPILabellingAnalysis(); });
-  am.registerPass([] { return MPIScopeAnalysis(); });
 
-  MPIScope &mpi_scope = am.getResult<MPIScopeAnalysis>(m);
   MPILabelling &mpi_labelling = am.getResult<MPILabellingAnalysis>(m);
 
-  errs() << "targ: " << targ << "\n";
-  // TODO: use the result of analyses -> rename this to SplitByRank pass
+  Instruction *comm_rank = mpi_labelling.get_unique_call("MPI_Comm_rank");
+  errs() << *comm_rank << "\n";
 
-  errs() << "TAG RANK: after\n";
+  if (comm_rank) {
+    CallSite cs_comm_rank(comm_rank);
+    Value *rank_val = cs_comm_rank.getArgument(1);
+    errs() << *rank_val << "\n";
+    errs() << "Users:\n";
+    for (auto *user : rank_val->users()) {
+      errs() << *user << "\n";
+    }
+    errs() << "Uses:\n";
+    for (auto &use : rank_val->uses()) {
+      errs() << *use << "\n";
+    }
 
-  return PreservedAnalyses::all();
+    // Function *fn_comm_rank = cs_comm_rank.getCalledFunction();
+    // if (fn_comm_rank->hasFnAttribute("rank")) {
+    //   errs() << "YES\n";
+    // } else {
+    //   errs() << "NO\n";
+    // }
+  }
+
+  // pruning code causes that all analyses are invalidated
+  return PreservedAnalyses::none();
 }

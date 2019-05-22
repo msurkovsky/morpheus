1a09526ad794d61e2f57daf95fbb30dd9991a074
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-05-22 17:08:46 +0200

    WIP:

--- src/morpheus.py
+++ src/morpheus.py
@@ -1,7 +1,8 @@
 import os
+import sys
 
 import click
-from plumbum import local
+from plumbum import local#, BG
 
 @click.command()
 @click.argument("source-file")
@@ -30,6 +31,14 @@ def generate_mpn(source_file, nproc, output_file):
     ]
 
     lib_morph = "libMorph.so"
+    # assert any(path in )
+
+    assert ("LD_LIBRARY_PATH" in os.environ), "LD_LIBRARY_PATH is not set."
+    assert any(
+        os.path.isfile(os.path.join(path, lib_morph))
+        for path in os.environ["LD_LIBRARY_PATH"].split(os.pathsep)
+    ), "Could not find libMorph.so. Check is LD_LIBRARY_PATH is set properly."
+
     opt_tool = "{}/opt".format(llvm_bin)
 
     # TODO: set LD_LIBRARY_PATH for libMorph.so .. assert this
@@ -52,7 +61,8 @@ def generate_mpn(source_file, nproc, output_file):
                 "-o", "-"
             ]
 
-            cmd = ll | a# | b
+            # cmd = (ll | a) & BG(stderr=sys.stderr)# | b
+            cmd = ll | a | b
             print(cmd())
 
 

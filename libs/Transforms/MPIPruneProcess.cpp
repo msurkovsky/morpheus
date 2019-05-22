84c44972961f205d5a0b3b816da8b803acb74637
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-05-22 20:53:38 +0200

    ENH: transformation pass to replace loads of rank by constant

--- libs/Transforms/MPIPruneProcess.cpp
+++ libs/Transforms/MPIPruneProcess.cpp
@@ -7,6 +7,8 @@
 #include "morpheus/Analysis/MPIScopeAnalysis.hpp"
 #include "morpheus/Transforms/MPIPruneProcess.hpp"
 
+#include <algorithm>
+
 using namespace llvm;
 
 // -------------------------------------------------------------------------- //
@@ -23,58 +25,24 @@ PreservedAnalyses MPIPruneProcessPass::run (Module &m, ModuleAnalysisManager &am
   MPILabelling &mpi_labelling = am.getResult<MPILabellingAnalysis>(m);
 
   Instruction *comm_rank = mpi_labelling.get_unique_call("MPI_Comm_rank");
-  errs() << *comm_rank << "\n";
 
   if (comm_rank) {
     CallSite cs_comm_rank(comm_rank);
     Value *rank_val = cs_comm_rank.getArgument(1);
-    errs() << *rank_val << "\n";
-    errs() << "Rank type: " << *rank_val->getType() << "\n";
-
-    LLVMContext &ctx = comm_rank->getFunction()->getContext();
-    IntegerType *t = IntegerType::get(ctx, 32);
-    IntegerType *t2 = TypeBuilder<types::i<32>, false>::get(ctx);
-
-    errs() << *t << " - " << *t2 << "\n";
 
     IRBuilder<> builder(comm_rank);
     ConstantInt *const_rank = builder.getInt32(rank_arg);
-    AllocaInst *const_rank_alloc = builder.CreateAlloca(builder.getInt32Ty(), nullptr, "rank");
-    builder.CreateStore(const_rank, const_rank_alloc);
 
-    errs() << "const_rank: " << *const_rank << "\n";
+    // NOTE: copy users to avoid iterator invalidation during replaces
+    std::vector<User *> users;
+    std::copy(rank_val->user_begin(), rank_val->user_end(), std::back_inserter(users));
 
-    errs() << "Users:\n";
-    for (auto *user : rank_val->users()) {
-      errs() << *user << "\n";
-    }
-
-    // rank_val->replaceAllUsesWith(const_rank_alloc); // NOTE: cannot use this, because it changes also MPI_Comm_rank call
-    for (auto *user : rank_val->users()) { // .. instead of replaceAllUses I'm using call on the user
-      if (user != comm_rank) {
-        user->replaceUsesOfWith(rank_val, const_rank_alloc);
+    for (auto *user : users) {
+      if (isa<LoadInst>(user)) {
+        // replace all loads from rank by const value
+        user->replaceAllUsesWith(const_rank);
       }
     }
-
-    errs() << "users of original rank:\n";
-    for (auto *user : rank_val->users()) {
-      errs() << *user << "\n";
-    }
-    errs() << "users of const rank:\n";
-    for (auto *user : const_rank_alloc->users()) {
-      errs() << *user << "\n";
-    }
-    // errs() << "Uses:\n";
-    // for (auto &use : rank_val->uses()) {
-    //   errs() << *use.getUser() << "\n";
-    // }
-
-    // Function *fn_comm_rank = cs_comm_rank.getCalledFunction();
-    // if (fn_comm_rank->hasFnAttribute("rank")) {
-    //   errs() << "YES\n";
-    // } else {
-    //   errs() << "NO\n";
-    // }
   }
 
   // pruning code causes that all analyses are invalidated

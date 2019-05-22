226f58b72cd761f9715935ea8df8f3e16d15c3fc
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-05-22 20:53:38 +0200

    WIP: prune process

--- libs/Transforms/MPIPruneProcess.cpp
+++ libs/Transforms/MPIPruneProcess.cpp
@@ -23,34 +23,37 @@ PreservedAnalyses MPIPruneProcessPass::run (Module &m, ModuleAnalysisManager &am
   MPILabelling &mpi_labelling = am.getResult<MPILabellingAnalysis>(m);
 
   Instruction *comm_rank = mpi_labelling.get_unique_call("MPI_Comm_rank");
-  errs() << *comm_rank << "\n";
 
   if (comm_rank) {
     CallSite cs_comm_rank(comm_rank);
     Value *rank_val = cs_comm_rank.getArgument(1);
-    errs() << *rank_val << "\n";
-    errs() << "Rank type: " << *rank_val->getType() << "\n";
 
-    LLVMContext &ctx = comm_rank->getFunction()->getContext();
-    IntegerType *t = IntegerType::get(ctx, 32);
-    IntegerType *t2 = TypeBuilder<types::i<32>, false>::get(ctx);
 
-    errs() << *t << " - " << *t2 << "\n";
+    // errs() << "comm rank: " << *comm_rank << "\n";
+    // auto it = comm_rank->getIterator();
+    // it++; // NOTE: i need to shift it to move further; why?
+    // Instruction *next_inst = &*it++;
+    // errs() << "next inst: " << *next_inst << "\n";
 
     IRBuilder<> builder(comm_rank);
     ConstantInt *const_rank = builder.getInt32(rank_arg);
     AllocaInst *const_rank_alloc = builder.CreateAlloca(builder.getInt32Ty(), nullptr, "rank");
     builder.CreateStore(const_rank, const_rank_alloc);
+    // builder.CreateStore(rank_val, const_rank);
 
     errs() << "const_rank: " << *const_rank << "\n";
 
     errs() << "Users:\n";
+    std::vector<User*> users;
     for (auto *user : rank_val->users()) {
+      users.push_back(user); // store users
       errs() << *user << "\n";
     }
 
     // rank_val->replaceAllUsesWith(const_rank_alloc); // NOTE: cannot use this, because it changes also MPI_Comm_rank call
-    for (auto *user : rank_val->users()) { // .. instead of replaceAllUses I'm using call on the user
+    for (auto *user : users) { // .. instead of replaceAllUses I'm using call on the user
+      // NOTE: iterate over stored users to avoid iterator invalidation
+      errs() << "USER: " << *user << "\n";
       if (user != comm_rank) {
         user->replaceUsesOfWith(rank_val, const_rank_alloc);
       }
@@ -60,10 +63,12 @@ PreservedAnalyses MPIPruneProcessPass::run (Module &m, ModuleAnalysisManager &am
     for (auto *user : rank_val->users()) {
       errs() << *user << "\n";
     }
+
     errs() << "users of const rank:\n";
     for (auto *user : const_rank_alloc->users()) {
       errs() << *user << "\n";
     }
+
     // errs() << "Uses:\n";
     // for (auto &use : rank_val->uses()) {
     //   errs() << *use.getUser() << "\n";

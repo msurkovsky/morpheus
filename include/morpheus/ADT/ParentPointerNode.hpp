e0608f4852327cc08eb01c019bbf04d1350309b0
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-05-27 10:44:27 +0200

    ENH+RF: implement call track within scope analysis and remove obsolte code

---
+++ include/morpheus/ADT/ParentPointerNode.hpp
@@ -0,0 +1,48 @@
+
+//===----------------------------------------------------------------------===//
+//
+// ParentPointerNode
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MRPH_PPNODE
+#define MRPH_PPNODE
+
+#include <memory>
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/raw_ostream.h"
+
+template<typename T>
+class PPNode {
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &, const PPNode<T> &);
+
+public:
+  T data;
+  std::shared_ptr<PPNode<T>> parent;
+  llvm::StringRef metadata = "X";
+
+  explicit PPNode(const T &data) : data(data) { }
+  PPNode(PPNode &&node) = default;
+  PPNode() = default;
+  PPNode(const PPNode &node) = delete;
+
+  template<typename... ArgsT>
+  static std::shared_ptr<PPNode<T>> create(ArgsT&&... args) {
+    return std::make_shared<PPNode<T>>(std::forward<ArgsT>(args)...);
+  }
+};
+
+namespace llvm {
+
+  template<typename T>
+  raw_ostream &operator<< (raw_ostream &out, const PPNode<T> &node) {
+    out << "Node(" << node.data << ")";
+    if (node.parent) {
+      out << " -> " << *node.parent;
+    } else {
+      out << ";";
+    }
+    return out;
+  }
+}
+#endif // MMRPH_PPNODE
--- libs/Analysis/MPIScopeAnalysis.cpp
+++ libs/Analysis/MPIScopeAnalysis.cpp
@@ -1,159 +1,102 @@
 
-#include "CallFinder.hpp"
-#include "morpheus/Analysis/MPILabellingAnalysis.hpp"
 #include "morpheus/Analysis/MPIScopeAnalysis.hpp"
 
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/IR/CallSite.h" // TODO: remove
-#include "llvm/IR/Dominators.h" // TODO: remove
-#include "llvm/Analysis/PostDominators.h" // TODO: remove
 #include "llvm/Support/raw_ostream.h"
 
 #include <cassert>
 #include <string>
-#include <utility>
-#include <map>
 
 using namespace llvm;
-using namespace std;
 
-// NOTE: Morpheus -> mor, mrp, mrh ... posible prefixes
+// -------------------------------------------------------------------------- //
+// MPIScopeAnalysis
 
 // The analysis runs over a module and tries to find a function that covers
 // the communication, either directly (MPI_Init/Finalize calls) or mediately
 // (via functions that calls MPI_Init/Finalize within)
 MPIScopeAnalysis::Result
-MPIScopeAnalysis::run(Module &m, ModuleAnalysisManager &am) {
+MPIScopeAnalysis::run(Module &m, ModuleAnalysisManager &mam) {
 
-  /*
-  CallGraph cg(m);
+  // TODO: this does not work!! -- causes run-time error
+  // FunctionAnalysisManager &fam = mam.getResult<FunctionAnalysisManagerModuleProxy>(m).getManager();
+  // fam.registerPass([]() { return MPILabellingAnalysis(); });
+  // auto res = fam.getCachedResult<MPILabellingAnalysis>(*main_fn);
 
-  string main_fname = "main";
+  // MPILabellingAnalysis la;
 
-  const Function *fmain;
-  CallGraphNode *cgn_main;
-  for (auto &cr : cg) {
-    if (cr.first != nullptr && cr.first->hasName() && cr.first->getName() == main_fname) {
-      errs() << "ABC:\n";
-      // errs() << *cr.first << "\n";
-      fmain = cr.first;
-      cgn_main = cr.second.get();
-      break;
-    }
-  }
+  std::shared_ptr<CallGraph> cg = std::make_shared<CallGraph>(m);
 
-  // errs() << "Fmain: " << *fmain << "\n";
-  CallGraphNode cgn(const_cast<Function *>(fmain));
-  cgn.stealCalledFunctionsFrom(cgn_main);
+  // MPILabelling labelling(cg);
+  // Instruction *i = labelling.get_unique_call("MPI_Finalize");
+  // errs() << *i << "\n";
 
-  cgn.print(errs());
-  errs() << "From graph: \n";
-  cgn_main->print(errs());
-  */
-
-  // Find the main function
-  Function *main_fn = nullptr;
-  for (auto &f : m) {
-    if (f.hasName() && f.getName() == "main") {
-      main_fn = &f;
-      break;
-    }
-  }
-
-  if (!main_fn) {
-    return MPIScopeResult(); // empty (invalid) scope result
-  }
-
-  errs() << "========\n";
-
-  FunctionAnalysisManager fam;
-
-  MPILabellingAnalysis la;
-  LabellingResult lr = la.run(*main_fn, fam); // TODO: I don't need to call run function. I can build a result directly. Or I can take FAM as proxy of MAM(!)
-  CallInst *ci = lr.get_unique_call("MPI_Init");
-
-  /*
-
-  if (lr.is_sequential(main_fn)) {
-    return MPIScopeResult();
-  }
-
-  errs() << "CI: " << *ci << "\n";
-  ImmutableCallSite ics(ci);
-  errs() << "Caller: " << *ics.getCaller() << "\n";
-  */
-  /*
-  Function *scope_candiate;
-  if (lr.does_invoke_call(main_fn, "MPI_Init")) {
-    scope_candiate = main_fn;
-  } else {
-    // indirect parallel calls
-    const std::vector<CallInst *> &indirect_calls = lr.get_indirect_mpi_calls(main_fn);
-    for (CallInst *inst : indirect_calls) {
-      Function *f = inst->getCalledFunction();
-      if (lr.does_invoke_call(f, "MPI_Init")) {
-        scope_candiate = f;
-        break;
-      } else {
-        // store  for further investigation
-      }
-    }
-  }
-  */
-
-
-  return MPIScopeResult();
-  /*
-  string init_f_name = "MPI_Init";
-  string finalize_f_name = "MPI_Finalize";
-
-
-  // ... it should be enough to have a parent for each call
-
-  // pair<CallInst *, CallInst *> caller_callee; // NOTE: to have an info who and where the callee was called
-
-  CallInst *mpi_init, *mpi_finalize; // The exact calls of MPI_Init/Finalize calls
-  CallInst *init_f, *finalize_f;
-
-  do {
-    for (auto &f : m) {
-      init_f = find_call_in_by_name(init_f_name, f);
-      finalize_f = find_call_in_by_name(finalize_f_name, f);
-
-      // Store mpi initi/finalize if found
-      // NOTE: the first appearance, i.e., null mpi_init,
-      //       means that init_f represents MPI_Init call.
-      if (!mpi_init && init_f) {
-        mpi_init = init_f;
-      }
-
-      if (!mpi_finalize && finalize_f) {
-        mpi_finalize = finalize_f;
-      }
-
-      // Looking for calls within one function that define the mpi scope
-      if (init_f && finalize_f) {
-        return Result(&f, init_f, finalize_f, mpi_init, mpi_finalize);
-      }
-
-      // If neither init_f nor finalize_f happened,
-      // then either 'init_f' or 'finalize_f' might happened.
-      if (init_f) {
-        StringRef f_name = f.getName();
-        assert(!f_name.empty()); // NOTE: called function has to have a name (actually it is not true but for now)
-        init_f_name = f_name;
-      } else if (finalize_f) {
-        StringRef f_name = f.getName();
-        assert(!f_name.empty());
-        finalize_f_name = f_name;
-      }
-    } // end for
-  } while(init_f || finalize_f); // continue search if at least one of the init/finalize call has been found.
-
-  return Result(); // return empty scope; MPI is not involved at all within the module
-  */
+  return MPIScope(cg);
 }
 
 // provide definition of the analysis Key
 AnalysisKey MPIScopeAnalysis::Key;
+
+// -------------------------------------------------------------------------- //
+// MPIScope
+
+MPIScope::MPIScope(std::shared_ptr<CallGraph> &cg)
+    : cg(cg),
+      labelling(std::make_unique<MPILabelling>(cg)) {
+
+  cg->print(errs());
+  /*
+  for (const auto &node : *cg) {
+    CallGraphNode const *cgn = node.second.get();
+    if (cgn->getFunction()) {
+        errs() << "FF: " << cgn->getFunction()->getName() << "\n";
+        CallsTrack ct = CallNode::create();
+        process_call_record(cgn, ct);
+    }
+  }
+
+  Instruction *inst = labelling->get_unique_call("MPI_Finalize");
+  errs() << *inst << "\n";
+
+  auto it = instructions_call_track.find(inst);
+  if (it != instructions_call_track.end()) {
+    CallsTrack ct = it->getSecond();
+    errs() << ct->metadata <<  " -> " << ct->parent->metadata << "\n";
+  }
+  */
+}
+
+// private members ---------------------------------------------------------- //
+
+MPIScope::CallsTrack
+MPIScope::process_call_record(CallGraphNode const *cgn, const CallsTrack &track) {
+
+
+
+  return CallNode::create();
+
+  /*
+  Instruction *inst = CallSite(cr.first).getInstruction();
+  auto it = instructions_call_track.find(inst);
+  if (it != instructions_call_track.end()) {
+    return it->getSecond();
+  }
+
+  CallsTrack res_track = CallNode::create(inst);
+  res_track->parent = track;
+  res_track->metadata = CallSite(inst).getCalledFunction()->getName();
+  instructions_call_track[inst] = res_track; // TODO: reason this storage (prevent recursive calls but is it ok?)
+
+  CallGraphNode *called_cgn = cr.second;
+  for (CallGraphNode::CallRecord const &inner_cr : *called_cgn) {
+    if (inner_cr.second->getFunction()) { // process only non-external nodes
+      errs() << "Last: " << inner_cr.second->getFunction()->getName() << "\n";
+      process_call_record(inner_cr, res_track);
+    }
+  }
+
+  return res_track;
+  */
+}

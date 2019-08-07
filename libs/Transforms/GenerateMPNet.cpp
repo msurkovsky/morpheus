fb37050e1fa29b23b7330bb075012a104a81b803
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-08-07 15:26:25 +0200

    RF: use updated names of CN in MPN generator

--- libs/Transforms/GenerateMPNet.cpp
+++ libs/Transforms/GenerateMPNet.cpp
@@ -25,26 +25,25 @@ PreservedAnalyses GenerateMPNetPass::run (Module &m, ModuleAnalysisManager &am) 
 
 
   // TODO: take rank/address value from the input code
-  AddressableCommNet acn(1);
+  AddressableCN acn(1);
 
   auto bfs_it = breadth_first(scope_fn);
   for (const BasicBlock *bb : bfs_it) {
     MPILabelling::MPICheckpoints checkpoints = mpi_labelling.get_mpi_checkpoints(bb);
-    errs() << bb << ": \n";
+    /* TODO: remove */ errs() << bb << ": \n";
     while (!checkpoints.empty()) {
       auto checkpoint = checkpoints.front();
       if (checkpoint.second == MPICallType::DIRECT) { // TODO: first solve direct calls
-        // std::unique_ptr<PluginCommNet> pcn = CommNetFactory::createCommNet(checkpoint.first);
-        acn.inject_pluign_cn(CommNetFactory::createCommNet(checkpoint.first));
-        // pcn->print(errs());
 
-        errs() << "\t" << *checkpoint.first.getInstruction() << "\n"; // instruction
+        PluginCNGeneric pcn = CNFactory::createCommSubnet(checkpoint.first);
+        std::move(pcn).inject_into(acn); // acn takes over the pcn elements
+
+        /* TODO: remove */ errs() << "\t" << *checkpoint.first.getInstruction() << "\n"; // instruction
         checkpoints.pop();
       }
     }
   }
 
-  acn.add_cf_edge(acn.entry_place(), acn.exit_place());
   acn.print(errs());
 
   return PreservedAnalyses::none(); // TODO: check which analyses have been broken?

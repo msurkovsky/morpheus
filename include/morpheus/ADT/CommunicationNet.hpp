6d6522cc931df3cf4fe240b31b2bdfa8047559ec
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-08-07 14:50:08 +0200

    ENH: add printable interface, expose net element types, and add AddressableCommNet

--- include/morpheus/ADT/CommunicationNet.hpp
+++ include/morpheus/ADT/CommunicationNet.hpp
@@ -8,6 +8,8 @@
 #ifndef MRPH_COMM_NET_H
 #define MRPH_COMM_NET_H
 
+#include "llvm/Support/raw_ostream.h"
+
 #include <memory>
 #include <string>
 #include <utility>
@@ -16,88 +18,243 @@
 
 using namespace std;
 
-class CommunicationNet {
-public:
-  enum EdgeType {
-    TAKE,
-    // just for input edges
-    FORCE_TAKE,
-    READ_ONLY,
-    FORCE_READY_ONLY,
-    SHUFFLE,
-  };
+namespace {
+
+enum EdgeType {
+  TAKE,
+  // just for input edges
+  FORCE_TAKE,
+  READ_ONLY,
+  FORCE_READY_ONLY,
+  SHUFFLE,
+};
+
+struct Printable {
+  virtual ~Printable() = default;
+
+  virtual void print (llvm::raw_ostream &os) const = 0;
+};
+
+llvm::raw_ostream &operator<< (llvm::raw_ostream &os, const Printable &printable) {
+  printable.print(os);
+  return os;
+}
+
+struct Element : public Printable {
+  using ID = unsigned int;
+
+  const ID id;
+  string name;
+
+  Element(string name) : id(get_id()), name(name) { }
+  Element(const Element &) = delete;
+  Element(Element &&) = default;
+
+  virtual void print (llvm::raw_ostream &os) const {
+    if (name.empty()) {
+      os << this;
+    } else {
+      os << name;
+    }
+  }
 
 private:
-  using ConditionList = vector<string>;
-
-  struct IEdge { };
-  template<class SrcT, class DestT>
-  struct EdgeT : IEdge {
-    SrcT src;
-    DestT dest;
-    EdgeType type;
-
-    explicit EdgeT(SrcT src, DestT dest, EdgeType type)
-      : src(src), dest(dest), type(type) { }
-    EdgeT(const EdgeT &) = delete;
-    EdgeT(EdgeT &&) = default;
-  };
-
-public:
-  struct PlaceT {
-    string name;
-    string type;
-    string init_expr;
-
-    explicit PlaceT(string name, string type, string init_expr)
-      : name(name), type(type), init_expr(init_expr) { }
-  };
-
-  struct TransitionT {
-    string name;
-    const ConditionList &conditions;
-
-    explicit TransitionT(string name, const ConditionList &cl)
-      : name(name), conditions(cl) { }
-  };
-
-  using PlaceRef = unique_ptr<PlaceT>&;
-  using TransitionRef = unique_ptr<TransitionT>&;
-
-  using InputEdgeT = EdgeT<PlaceRef, TransitionRef>;
-  using OutputEdgeT = EdgeT<TransitionRef, PlaceRef>;
-
-private:
-  vector<unique_ptr<PlaceT>> places;
-  vector<unique_ptr<TransitionT>> transitions;
-
-  vector<unique_ptr<InputEdgeT>> input_edges;
-  vector<unique_ptr<OutputEdgeT>> output_edges;
-  vector<unique_ptr<IEdge>> control_flow_edges;
-
-public:
-  PlaceRef add_place(string type, string init_expr, string name="") {
-    places.push_back(make_unique<PlaceT>(name, type, init_expr));
-    return places.back();
-  }
-
-  TransitionRef add_transition(ConditionList &cl, string name="") {
-    transitions.push_back(make_unique<TransitionT>(name, cl));
-    return transitions.back();
-  }
-
-  void add_input_edge(PlaceRef src, TransitionRef dest, EdgeType type=TAKE) {
-    input_edges.push_back(make_unique<InputEdgeT>(src, dest, type));
-  }
-
-  void add_output_edge(TransitionRef src, PlaceRef dest) {
-    output_edges.push_back(make_unique<OutputEdgeT>(src, dest, TAKE));
-  }
-
-  template<class SrcT, class DestT>
-  void add_cf_edge(SrcT src, DestT dest) {
-    control_flow_edges.push_back(make_unique<EdgeT<SrcT, DestT>>(src, dest, TAKE));
+  static ID get_id() {
+    static ID id = 0;
+    return ++id;
   }
 };
+
+
+struct Edge : public Printable {
+  const Element &startpoint;
+  const Element &endpoint;
+  EdgeType type;
+
+  explicit Edge(const Element &startpoint, const Element &endpoint, EdgeType type)
+    : startpoint(startpoint), endpoint(endpoint), type(type) { }
+
+  Edge(const Edge &) = delete;
+  Edge(Edge &&) = default;
+
+  void print (llvm::raw_ostream &os) const {
+    os << startpoint << " -> " << endpoint;
+  }
+};
+
+
+struct Place : Element {
+  string type;
+  string init_expr;
+
+  explicit Place(string name, string type, string init_expr)
+    : Element(name), type(type), init_expr(init_expr) { }
+
+  Place(const Place &) = delete;
+  Place(Place &&) = default;
+  ~Place() = default;
+
+  void print (llvm::raw_ostream &os) const {
+    os << "P(" << id << "): ";
+
+    Element::print(os);
+
+    os << "<";
+    if (!type.empty()) {
+      os << type;
+    }
+    os << ">";
+
+    os << "[";
+    if (!init_expr.empty()) {
+      os << init_expr;
+    }
+    os << "]";
+  }
+};
+
+
+using ConditionList = vector<string>;
+
+struct Transition : Element {
+  string name;
+  const ConditionList guard;
+
+  explicit Transition(string name, const ConditionList guard)
+    : Element(name), guard(guard) { }
+
+  Transition(const Transition &) = delete;
+  Transition(Transition &&) = default;
+  ~Transition() = default;
+
+  void print (llvm::raw_ostream &os) const {
+    os << "T(" << id << "): ";
+
+    Element::print(os);
+
+    os << "[";
+    if (std::distance(guard.begin(), guard.end()) > 0) {
+      auto it = guard.begin();
+      for (; std::distance(it, guard.end()) > 1; it++) {
+        os << *it << ", ";
+      }
+      os << *it;
+    }
+    os << "]";
+  }
+};
+
+
+// ==============================================================================
+
+class CommunicationNet : public Printable {
+  vector<unique_ptr<Place>> places;
+  vector<unique_ptr<Transition>> transitions;
+
+  vector<unique_ptr<Edge>> input_edges;
+  vector<unique_ptr<Edge>> output_edges;
+  vector<unique_ptr<Edge>> control_flow_edges;
+
+public:
+  CommunicationNet() = default;
+  CommunicationNet(const CommunicationNet &) = delete;
+  CommunicationNet(CommunicationNet &&) = default;
+
+  Place const *add_place(string type, string init_expr, string name="") {
+    places.push_back(make_unique<Place>(name, type, init_expr));
+    return places.back().get();
+  }
+
+  Transition const *add_transition(ConditionList cl, string name="") {
+    transitions.push_back(make_unique<Transition>(name, cl));
+    return transitions.back().get();
+  }
+
+  Edge const *add_input_edge(const Place &src, const Transition &dest, EdgeType type=TAKE) {
+    input_edges.push_back(make_unique<Edge>(src, dest, type));
+    return input_edges.back().get();
+  }
+
+  Edge const *add_output_edge(const Transition &src, const Place &dest) {
+    output_edges.push_back(make_unique<Edge>(src, dest, TAKE));
+    return output_edges.back().get();
+  }
+
+  template<typename Startpoint, typename Endpoint>
+  Edge const *add_cf_edge(const Startpoint& src, const Endpoint& dest) {
+    control_flow_edges.push_back(make_unique<Edge>(src, dest, TAKE));
+    return control_flow_edges.back().get();
+  }
+
+  virtual void print (llvm::raw_ostream &os) const {
+    os << "Places:\n";
+    for (auto &p : places) {
+      os << "  " << *p << "\n";
+    }
+    os << "Transitions:\n";
+    for (auto &t : transitions) {
+      os << "  " << *t << "\n";
+    }
+    os << "Input edges:\n";
+    for (auto &e : input_edges) {
+      os << "  " << *e << "\n";
+    }
+    os << "Outuput edges:\n";
+    for (auto &e : output_edges) {
+      os << "  " << *e << "\n";
+    }
+    os << "CF edges:\n";
+    for (auto &e : control_flow_edges) {
+      os << "  " << *e << "\n";
+    }
+  }
+};
+
+class PluginCommNet : public CommunicationNet {
+
+};
+
+class AddressableCommNet : public CommunicationNet {
+
+  using Address = unsigned int;
+
+  const Address address;
+  const Place &asr;
+  const Place &arr;
+  const Place &csr;
+  const Place &crr;
+
+public:
+  AddressableCommNet(Address address)
+    : address(address),
+      asr(*add_place("MessageToken", "", "Active Send Request")),
+      arr(*add_place("MessageRequest", "", "Active Receive Request")),
+      csr(*add_place("MessageRequest", "", "Completed Send Request")),
+      crr(*add_place("MessageToken", "", "Completed Receive Request")) { }
+
+  AddressableCommNet(const AddressableCommNet &) = delete;
+  AddressableCommNet(AddressableCommNet &&) = default;
+
+  struct DummyT { };
+  using WhereSpec = DummyT;
+  using SubCN = DummyT;
+
+  void emplace(WhereSpec wherespec, SubCN& sub_cn) {
+    // connect the sub-comm. net into a place
+
+    // TODO: Q: do I need something special from SubCN or can I use CommunicationNet itself directly?
+    //       Q: what is better to enhance CN or define new "extended type?"
+  }
+
+  void print (llvm::raw_ostream &os) {
+    os << "Adress: " << address << "\n";
+    os << "------------------------------------------------------------\n";
+    CommunicationNet::print(os);
+    os << "------------------------------------------------------------\n\n";
+  }
+};
+
+
+} // end of anonymous namespace
 
 #endif // MRPH_COMM_NET_H

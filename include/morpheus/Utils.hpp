8bf109fb2760f75d41b71f5e5f400e3c96ebca2c
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-08-07 14:53:39 +0200

    ENH: utils - pretty print vector

--- include/morpheus/Utils.hpp
+++ include/morpheus/Utils.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <sstream>
 
+namespace {
+
 template<typename T>
 void store_if_not(std::vector<T> &values, const T &v, T excl) {
   if (v != excl) {
@@ -14,21 +16,31 @@ void store_if_not(std::vector<T> &values, const T &v, T excl) {
 }
 
 template<typename T>
-std::string format_tuple(const std::vector<T> &values) {
-  std::ostringstream str;
-  if (values.size() == 1) {
-    str << values[0];
-  } else if (values.size() > 1) {
-    str << "(";
-    auto it = values.begin();
-    str << *it; it++;
-    for (; it != values.end(); it++) {
-      str << ", " << *it;
-    }
-    str << ")";
-    return str.str();
+std::string pp_vector(const std::vector<T> &values,
+                      std::string delim=",",
+                      std::string lbracket="",
+                      std::string rbracket="") {
+  std::ostringstream oss;
+  if (values.empty()) {
+    return "";
   }
-  return str.str();
+
+  if (values.size() == 1) {
+    oss << values[0];
+    return oss.str();
+  }
+
+  // more than one value
+  oss << lbracket;
+  auto it = values.begin();
+  oss << *it; it++;
+  for (; it != values.end(); it++) {
+    oss << delim << *it;
+  }
+  oss << rbracket;
+  return oss.str();
 }
+
+} // end of anonymous namespace
 
 #endif // MRPH_UTILS_H

2846d16143e60af96888ba018948c4b9a14a0c2d
Martin Surkovsky <martin.surkovsky@gmail.com>
2019-08-17 22:38:44 +0200

    ENH: bind all inluded paths to special remotes when run via docker

--- morpheus-docker-run.py
+++ morpheus-docker-run.py
@@ -11,7 +11,8 @@ from plumbum import local
 @click.argument("source-file")
 @click.option("-np", "--nproc", default=1, help="Number of processes.")
 @click.option("-o", "--output-dir", default=None, type=str, help="Output directory")
-def run(source_file, nproc, output_dir):
+@click.option("-I", "--includes", default=None, type=str, multiple=True, help="Add directory to include search path")
+def run(source_file, nproc, output_dir, includes):
     src_path = os.path.abspath(source_file)
     src_dir_path = os.path.abspath(os.path.dirname(source_file))
     src_file_basename = os.path.basename(source_file)
@@ -19,20 +20,31 @@ def run(source_file, nproc, output_dir):
         output_dir = os.getcwd()
     dest_path = os.path.abspath(output_dir)
 
+    # prepare includes
+    incls = [] if includes is None else includes[:]
+    n = iter(range(len(incls)))
+    get_remote = lambda: "/remote{}".format(next(n))
+    paths_to_remotes = [(os.path.abspath(path), "{}".format(get_remote()))
+                        for path in incls]
+
+    # put together all remotes
+    remotes = [(src_dir_path, "/input"), (dest_path, "/output")] + paths_to_remotes
+
+    flatten = lambda l: [item for sublist in l for item in sublist]
+
     user_id = pwd.getpwnam(os.environ["USER"]).pw_uid
     cmd = local["docker"][
         "run",
-        "--user", user_id,
-        "-v", "{}:{}".format(src_dir_path, "/input"),
-        "--user", user_id,
-        "-v", "{}:{}".format(dest_path, "/output"),
+        flatten(("--user", user_id, "-v", remote)
+                for remote in map(lambda x: "{}:{}".format(*x), remotes)),
         "morpheus",
         "-np", nproc,
         os.path.join("/input", src_file_basename),
-        "-o", "/output"
+        "-o", "/output",
+        flatten(("-I", path_to_remote[1]) for path_to_remote in paths_to_remotes)
     ]
 
-    cmd();
+    cmd()
 
 if __name__ == "__main__":
     run()
